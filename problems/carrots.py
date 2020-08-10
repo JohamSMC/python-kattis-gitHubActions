3f3ac335e7c693e9576db645aa8b274ac36c9b8f
Joham Medina <joham.nba@gmail.com>
2020-08-09 22:22:28 -0500

    test 6 gitHub Actions

--- problems/carrots.py
+++ problems/carrots.py
@@ -1,9 +1,6 @@
-import os
-
 def main():
 	output=input().split()[1]
 	print(output)	
 	
-if __name__ == '__main__':
-	path_file = os.path.dirname(os.path.abspath(__file__))
+if __name__ == '__main__':	
 	main()	
