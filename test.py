45e31c7fd6ab9dc7320a338f2f41f83bdf7043b2
Joham Medina <joham.nba@gmail.com>
2020-08-11 13:22:33 -0500

    Add Hello problem solution

--- test.py
+++ test.py
@@ -9,5 +9,11 @@ class TestCarrots(unittest.TestCase):
         self.assertEqual(int(str(output).split("'")[1].split("\\n")[0]), 1)
         output = subprocess.check_output("python problems/carrots.py < inputs/inputCarrots2.txt", shell=True)
         self.assertEqual(int(str(output).split("'")[1].split("\\n")[0]), 5)
+
+    def test_hello(self):
+    	import problems.hello
+    	output = subprocess.check_output("python problems/hello.py", shell=True)
+    	self.assertEqual(str(output).split("'")[1].split("\\n")[0], "Hello World!")
+    	
 if __name__ == "__main__":
     unittest.main()
